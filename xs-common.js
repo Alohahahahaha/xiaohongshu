33dff4817cc20e69aa8f1bcf3850741df152c008
Alohahahahaha <2357349368@qq.com>
2024-10-15 18:51:35 +0800

    i++

---
+++ xs-common.js
@@ -0,0 +1,483 @@
+/*
+@Author: Aloha
+@Time: 2024/10/14 1:14
+@ProjectName: brandnew
+@FileName: x-common.py
+@Software: PyCharm
+*/
+
+var encrypt_lookup = ['Z', 'm', 's', 'e', 'r', 'b', 'B', 'o', 'H', 'Q', 't', 'N', 'P', '+', 'w', 'O', 'c', 'z', 'a', '/', 'L', 'p', 'n', 'g', 'G', '8', 'y', 'J', 'q', '4', '2', 'K', 'W', 'Y', 'j', '0', 'D', 'S', 'f', 'd', 'i', 'k', 'x', '3', 'V', 'T', '1', '6', 'I', 'l', 'U', 'A', 'F', 'M', '9', '7', 'h', 'E', 'C', 'v', 'u', 'R', 'X', '5']
+
+function a0_0x4562() {
+    var t = ["YYoSR", "rhOyV", "sxbAo", "aTuAi", "xajtV", "gQbxB", "UmpWx", "ddswA", "kxwhd", "QMvnM", "KabsY", "HoqDm", "fXpGx", "MxRSL", "Twima", "zAnEW", "rqcQI", "join", "userAge", "ZrfCV", "gmYiF", "aToRy", "sfXtM", "A4NjFqY", "kQdeo", "GyHQU", "TUMma", "floor", "String", "cVte9UJ", "lIGVb", "ule", "vQcMj", "HFfzT", "bjZLJ", "getTime", "khMzy", "AIvJY", "ezvVk", "VWXYZab", "HioSg", "14047796pmFjxA", "YQHEl", "AAHOg", "MVfpC", "rpOJZ", "zDsSM", " Object", "AKqWJ", "TmtvD", "x3VT16I", "|7|6|3|", "6|5|3|7", "mqwTT", "oRfEy", "ZUqtM", "AMpFN", "lZzjg", "uanbD", "encodin", "ADzBt", "GAAFA", "vXYvp", "RDgnu", "UZGLM", "DYGiC", "OTDmr", "oLnZX", "UOvBB", "navigat", "moTVi", "__esMod", "KblCWi+", "nUIaV", "AbWfM", "push", "asStrin", "OPQRSTU", "syPaZ", "kOtZv", "WBzOj", "QtAKi", "dQJvJ", "twMTN", "RpTrd", "readFlo", "_hh", "iliLS", "nfWyB", "YQOUV", "Words", "zKqdy", "3|5|2|0", "slice", "cUEKO", "kdtls", "fnCys", "YqUgj", "TukVg", "wYcVv", "VbiwY", "pfBOc", "ZSFiZ", "UoRga", "eUqJS", "jFuNn", "size", "oZFcs", "YOtxY", "csCcj", "LpmsP", "aNnru", "HuPPA", "fuWik", "0XTdDgM", "YeksC", "stringi", "TWRRC", "QodQz", "oEJOH", "ZTzUA", "grmRY", "rotl", "WeJku", "166608rPheNY", "asBytes", "nsRVs", "TqqUf", "toStrin", "znhDm", "rable", "QvcwX", "kAztI", "kJbJF", "nxLqG", "functio", "[object", "vMgSY", "u5wPHsO", "charCod", "|1|8|2|", "THbLr", "wxFrF", "JOGcc", "kieAy", "kLHfg", "_blocks", "LtJdC", "lpzNX", "Illegal", "zrCYZ", "exports", "QLCpu", "oAuNK", "0DSfdik", "fMcPH", "aQDqP", "cdefghi", "456789+", "luphr", "eAt", "|4|7|9|", "hECvuRX", "GBbjK", "_ff", "EtoLj", "RkUqN", "XiASu", "yRnhISG", "DaKOp", "cEthh", "lDyWO", "mYFfI", "747000bpoxxx", "uPINO", "hasOwnP", "OvSbZ", "utf8", "NSbRM", "678834DtOObi", "WiEUW", "oHQtNP+", "jxQfH", "TrFCp", "PeUOX", "RhBYA", "VVqhr", "Aqnje", "5|2|6", "XvsAD", "LISkf", "UpJqT", "KpdHM", "EhmqT", "QVfWN", "constru", "alert", "ymSZq", "Bvk6/7=", "Hex", "indexOf", "4|0", "OyLZz", "EjhiU", "oUlNk", "iamspam", "lePWA", "replace", "RlUQG", "nt ", "endian", "isBuffe", "tvTuG", "length", "oBytes", "oUtLs", "KIBVg", "EfOwb", "dyihR", "QvByC", "lVsog", "HWloQ", "ukdmU", "charAt", "aiPCy", " argume", "nSvaa", "vpnsk", "0|1|8|3", "xyz0123", "oZrdv", "|1|4", "vCMhQ", "dTDnB", "HIJKLMN", "stringT", "undefin", "2983832yCfWmB", "MVMuH", "rCode", "a2r1ZQo", "pFhMH", "VzRME", "LbIVv", "vdWST", "SCUrw", "LpfE8xz", "_gg", "pUeCm", "IkSKP", "XMnZK", "isipE", "UuTzz", "substr", "wBwKE", "VWADf", "iRRsj", "cwtZh", "bin", "QDDcY", "KPDsF", "ctor", "dZDTV", "enumera", "_isBuff", "RuOzo", "CaLuR", "dqkPn", "oKmZD", "POmcO", "rZygc", "KCdCY", "UVUok", "dHVwa", "get", "DAxHG", "pBFAI", "IXUYL", "jgCKA", "bytesTo", "OJbNp", "fromCha", "|2|6|7|", "string", "binary", "q42KWYj", "YadeM", "WpMRH", "roperty", "PnfZL", "random", "sxJcO", "aILdC", "ABCDEFG", "split", "jklmnop", "wrjdQ", "RChcL", "Korlo", "lUAFM97", "1|5|0|3", "rLPoA", "fsfWX", "LZldE", "dXPDy", "configu", "fHaED", "OjOSM", "defineP", "PKMEb", "NUDjq", "RoYii", "bZadJ", "pow", "ugRAu", "QtAXc", "ize", "ZmserbB", " Array]", "tiWCE", "ApyeS", "YjOdD", "default", "MbCqK", "dWDRM", "eqtAt", "isNoh", "_ii", "mQftZ", "wordsTo", "yOCos", "YazxT", "dYrlI", "rbvpK", "Fxjya", "call", "_digest", "pngG8yJ", "zuxrg", "xycJU", "KGBPZ", "dwkbK", "nmEYA", "AIwtp", "HoRIs", "eiUxV", "isArray", "XjFqq", "YLkkY", "dIINx", "avjRj", "XiUBb", "IFlYW", "wOcza/L", "1108521TgMXYJ", "XRaRa", "UfZiV", "test", "GIlRJ", "YxRuc", "mDArr", "5|4|0|1", "5303600OvAwBy", "tQrlu", "PJMqq", "oRPrz", "pxmgV", "eiErr", "Bytes", "xBGAU", "PkOgS", "qrstuvw", "nBYdB", "uYjCR", "prototy", "rAlBp", "atLE", "DJmdg", "FQqah", "Dxojc", "oTyhe", "eMwyL", "AeCLB", "IEDmq", "ble", "dAKMZ", "faved", "xxfUq", "DiTqT", "GBVeZ", "KozBG", "VCkef"];
+    return (a0_0x4562 = function () {
+            return t
+        }
+    )()
+}
+
+function a0_0x2092(t, e) {
+    var r = a0_0x4562();
+    return (a0_0x2092 = function (t, e) {
+            return r[t -= 194]
+        }
+    )(t, e)
+}
+
+function a0_0x2923d6(t, e) {
+    return a0_0x2092(e - 299, t)
+}
+
+var encrypt_mcr = function (t) {
+    var e = 409
+        , r = 514
+        , n = 292
+        , o = 642
+        , i = 447
+        , a = 422
+        , u = 582
+        , s = 546
+        , c = 491
+        , l = 364
+        , f = 511
+        , p = 736
+        , h = 543
+        , d = 203
+        , v = 380
+        , g = 428
+        , m = 579
+        , y = 397
+        , w = 396
+        , _ = 228
+        , b = 396
+        , E = 184
+        , T = 148
+        , k = 30
+        , x = 323
+        , S = 503
+        , A = 212
+        , L = 186
+        , R = 370
+        , I = 321
+        , C = 430
+        , O = 397
+        , N = 559
+        , P = 376
+        , M = 445
+        , B = 230
+        , F = 264
+        , j = 137
+        , D = 475
+        , U = 508
+        , q = 57
+        , G = 119
+        , H = 295
+        , V = 167
+        , W = 135
+        , z = 312
+        , X = 318
+        , K = 194
+        , Y = 1179
+        , J = {};
+    J[Z(-578, -445)] = function (t, e) {
+        return t === e
+    }
+        ,
+        J[Z(-348, -e)] = Z(-681, -r),
+        J[Z(-n, -473)] = function (t, e) {
+            return t ^ e
+        }
+        ,
+        J[Z(-o, -i)] = function (t, e) {
+            return t ^ e
+        }
+        ,
+        J[Z(-a, -u)] = function (t, e) {
+            return t & e
+        }
+        ,
+        J[Z(-s, -c)] = function (t, e) {
+            return t >>> e
+        }
+        ,
+        J[Z(-l, -f)] = function (t, e) {
+            return t ^ e
+        }
+        ,
+        J[Z(-p, -h)] = function (t, e) {
+            return t < e
+        }
+        ,
+        J[Z(-d, -v)] = function (t, e) {
+            return t ^ e
+        }
+        ,
+        J[Z(-g, -396)] = function (t, e) {
+            return t >>> e
+        }
+        ,
+        J[Z(-437, -m)] = function (t, e) {
+            return t ^ e
+        }
+    ;
+    var $ = J
+        , Q = 3988292384;
+
+    function Z(t, e) {
+        return a0_0x2923d6(t, e - -Y)
+    }
+
+    for (var tt, et, rt = 256, nt = []; rt--; nt[rt] = tt >>> 0)
+        for (et = 8,
+                 tt = rt; et--;)
+            tt = 1 & tt ? $[Z(-y, -w)](tt, 1) ^ Q : $[Z(-_, -b)](tt, 1);
+    return function (t) {
+        var e = 261;
+
+        function r(t, r) {
+            return Z(r, t - e)
+        }
+
+        if ($[r(-E, -143)]('1', '1')) {
+            for (var n = 0, o = -1; n < t.length; ++n)
+                o = $[r(-A, -252)](nt[$[r(-L, -R)]($[r(-I, -C)](o, 255), t[r(-O, -N) + r(-P, -M)](n))], $[r(-B, -F)](o, 8));
+            return $[r(-250, -j)](o, -1) ^ Q
+        }
+        for (n = 0,
+                 o = -1; $[r(-282, -D)](n, t[r(-323, -U)]); ++n)
+            o = $[r(-119, q)](nt[$[r(-G, -H)]($[r(-I, -V)](o, 255), t[n])], $[r(-W, -z)](o, 8));
+        return $[r(-X, -K)](o, -1) ^ Q
+    }
+}()
+
+function getSigCount(t) {
+    var e = Number(window.sessionStorage.getItem('sc')) || 0;
+    return t && (e++,
+        sessionStorage.setItem('sc', e.toString())),
+        e
+}
+
+function encrypt_encodeUtf8(t) {
+    var e = 206
+        , r = 87
+        , n = 85
+        , o = 109
+        , i = 129
+        , a = 254
+        , u = 106
+        , s = 106
+        , c = 115
+        , l = 77
+        , f = 255
+        , p = 151
+        , h = 5
+        , d = 300
+        , v = 138
+        , g = 347
+        , m = 190
+        , y = 165
+        , w = 169
+        , _ = {
+        OJbNp: function (t, e) {
+            return t(e)
+        },
+        vdWST: function (t, e) {
+            return t === e
+        },
+        OTDmr: function (t, e) {
+            return t + e
+        },
+        UuTzz: function (t, e) {
+            return t + e
+        },
+        nfWyB: function (t, e, r) {
+            return t(e, r)
+        }
+    }
+        , b = _[E(-161, -49)](encodeURIComponent, t);
+
+    function E(t, e) {
+        return a0_0x2923d6(t, e - -711)
+    }
+
+    for (var T = [], k = 0; k < b[E(-e, -116)]; k++) {
+        var x = b[E(-74, -106)](k);
+        if (_[E(r, -n)](x, "%")) {
+            var S = _[E(o, i)](b[E(-a, -u)](k + 1), b[E(-22, -s)](_[E(c, -l)](k, 2)))
+                , A = _[E(f, p)](parseInt, S, 16);
+            T[E(-h, 138)](A),
+                k += 2
+        } else
+            T[E(d, v)](x[E(-g, -m) + E(-y, -w)](0))
+    }
+    return T
+}
+
+function encrypt_tripletToBase64(t) {
+    var e = 975
+        , r = 1117
+        , n = 1335
+        , o = 1277
+        , i = 1263
+        , a = 1188
+        , u = 1306
+        , s = 1444
+        , c = 1363
+        , l = 1324
+        , f = 1050
+        , p = 1171
+        , h = 1180
+        , d = 1263
+        , v = 1155
+        , g = 1306
+        , m = 1355
+        , y = 966
+        , w = 1149
+        , _ = {};
+
+    function b(t, e) {
+        return a0_0x2923d6(t, e - 551)
+    }
+
+    _[b(1101, 1086)] = function (t, e) {
+        return t + e
+    }
+        ,
+        _[b(e, r)] = function (t, e) {
+            return t + e
+        }
+        ,
+        _[b(1526, n)] = function (t, e) {
+            return t >> e
+        }
+        ,
+        _[b(o, i)] = function (t, e) {
+            return t & e
+        }
+        ,
+        _[b(a, u)] = function (t, e) {
+            return t >> e
+        }
+        ,
+        _[b(s, c)] = function (t, e) {
+            return t >> e
+        }
+        ,
+        _[b(l, 1149)] = function (t, e) {
+            return t & e
+        }
+    ;
+    var E = _;
+    return E[b(f, 1086)](E[b(1102, 1117)](encrypt_lookup[63 & E[b(p, 1335)](t, 18)], encrypt_lookup[E[b(h, d)](E[b(v, g)](t, 12), 63)]) + encrypt_lookup[E[b(1382, d)](E[b(m, c)](t, 6), 63)], encrypt_lookup[E[b(y, w)](t, 63)])
+}
+
+function encrypt_encodeChunk(t, e, r) {
+    var n, o = 50, i = 229, a = 178, u = 3, s = 89, c = 161, l = 28, f = 97, p = 28, h = 46, d = 142, v = 19, g = 37,
+        m = 0, y = {
+            WiEUW: function (t, e) {
+                return t < e
+            },
+            Twima: function (t, e) {
+                return t + e
+            },
+            rpOJZ: function (t, e) {
+                return t & e
+            },
+            oRfEy: function (t, e) {
+                return t(e)
+            }
+        };
+
+    function w(t, e) {
+        return a0_0x2923d6(t, e - -791)
+    }
+
+    for (var _ = [], b = e; y[w(-o, -i)](b, r); b += 3)
+        n = y[w(-a, -u)](y[w(-s, 28)](t[b] << 16, 16711680), y[w(c, l)](t[b + 1] << 8, 65280)) + y[w(f, p)](t[y[w(-h, -u)](b, 2)], 255),
+            _[w(d, 58)](y[w(v, g)](encrypt_tripletToBase64, n));
+    return _[w(99, m)]("")
+}
+
+function encrypt_b64Encode(t) {
+    var e = 318
+        , r = 252
+        , n = 296
+        , o = 191
+        , i = 300
+        , a = 40
+        , u = 36
+        , s = 226
+        , c = 272
+        , l = 263
+        , f = 380
+        , p = 35
+        , h = 74
+        , d = 9
+        , v = 295
+        , g = 307
+        , m = 298
+        , y = 124
+        , w = 49
+        , _ = 5
+        , b = 139
+        , E = 157
+        , T = 236
+        , k = 145
+        , x = 265
+        , S = 30
+        , A = 103
+        , L = 88
+        , R = 296
+        , I = 28
+        , C = 190
+        , O = 32
+        , N = 216
+        , P = 36
+        , M = 51
+        , B = 20
+        , F = 218
+        , j = 224
+        , D = 263
+        , U = 447
+        , q = 81
+        , G = 161
+        , H = 237
+        , V = 36
+        , W = 212
+        , z = 295
+        , X = 130
+        , K = 46
+        , Y = 78
+        , J = 479
+        , $ = 306
+        , Q = 441
+        , Z = 358
+        , tt = 124
+        , et = 279
+        , rt = 134
+        , nt = 821
+        , ot = {};
+
+    function it(t, e) {
+        return a0_0x2923d6(e, t - -nt)
+    }
+
+    ot[it(-e, -344)] = function (t, e) {
+        return t % e
+    }
+        ,
+        ot[it(-88, -r)] = function (t, e) {
+            return t - e
+        }
+        ,
+        ot[it(-n, -160)] = function (t, e) {
+            return t < e
+        }
+        ,
+        ot[it(-o, -i)] = function (t, e) {
+            return t > e
+        }
+        ,
+        ot[it(-a, -174)] = function (t, e) {
+            return t + e
+        }
+        ,
+        ot[it(-u, -s)] = function (t, e) {
+            return t === e
+        }
+        ,
+        ot[it(-218, -c)] = function (t, e) {
+            return t + e
+        }
+        ,
+        ot[it(-l, -f)] = function (t, e) {
+            return t >> e
+        }
+        ,
+        ot[it(p, -h)] = function (t, e) {
+            return t & e
+        }
+        ,
+        ot[it(-161, d)] = function (t, e) {
+            return t << e
+        }
+        ,
+        ot[it(-v, -g)] = function (t, e) {
+            return t << e
+        }
+        ,
+        ot[it(-306, -m)] = function (t, e) {
+            return t + e
+        }
+        ,
+        ot[it(-y, 38)] = function (t, e) {
+            return t >> e
+        }
+        ,
+        ot[it(w, -_)] = function (t, e) {
+            return t & e
+        }
+    ;
+    for (var at = ot, ut = (it(-b, 13) + it(-E, -T) + "4")[it(-k, -66)]("|"), st = 0; ;) {
+        switch (ut[st++]) {
+            case "0":
+                var ct = at[it(-e, -x)](ht, 3);
+                continue;
+            case "1":
+                var lt;
+                continue;
+            case "2":
+                var ft = 16383;
+                continue;
+            case "3":
+                var pt = [];
+                continue;
+            case "4":
+                return pt[it(-S, 128)]("");
+            case "5":
+                var ht = t[it(-226, -A)];
+                continue;
+            case "6":
+                for (var dt = 0, vt = at[it(-L, 65)](ht, ct); at[it(-R, -236)](dt, vt); dt += ft)
+                    pt[it(I, C)](encrypt_encodeChunk(t, dt, at[it(-o, -O)](dt + ft, vt) ? vt : at[it(-a, -N)](dt, ft)));
+                continue;
+            case "7":
+                at[it(-P, -155)](ct, 1) ? (lt = t[ht - 1],
+                    pt[it(I, M)](at[it(-40, -B)](at[it(-F, -j)](encrypt_lookup[at[it(-D, -U)](lt, 2)], encrypt_lookup[at[it(p, -q)](at[it(-G, -H)](lt, 4), 63)]), "=="))) : at[it(-V, -W)](ct, 2) && (lt = at[it(-z, -X)](t[ht - 2], 8) + t[at[it(-L, -K)](ht, 1)],
+                    pt[it(I, Y)](at[it(-306, -J)](at[it(-$, -281)](at[it(-306, -Q)](encrypt_lookup[at[it(-D, -Z)](lt, 10)], encrypt_lookup[63 & at[it(-tt, -et)](lt, 4)]), encrypt_lookup[at[it(w, -rt)](at[it(-295, -274)](lt, 2), 63)]), "=")));
+                continue
+        }
+        break
+    }
+}
+
+function xsCommon(t, s) {
+    var c = t
+        , l = s
+        , f = ""
+        , p = getSigCount(c && l || f)
+        , h = window.localStorage.getItem("b1")
+        , d = window.localStorage.getItem("b1b1") || "1"
+        , v = {
+        s0: 5,
+        s1: "",
+        x0: d,
+        x1: "3.7.8-2",
+        x2: "Windows",
+        x3: "xhs-pc-web",
+        x4: "4.34.0",
+        x5: "18e88f41f71tejrvpve7quf2bctylrbfgcm6ni30v50000344101",
+        x6: c,
+        x7: l,
+        x8: h,
+        x9: encrypt_mcr(''.concat(c).concat(l).concat(h)),
+        x10: p
+    }
+        , xs_common = encrypt_b64Encode(encrypt_encodeUtf8(JSON.stringify(v)))
+    var params = {
+        'X-s': s,
+        'X-t': t,
+        'X-S-Common': xs_common
+    }
+    return params
+}
